c817aef32ea93eddd115681ba63775a9cef7bac6
Julian <lgldppst@gmail.com>
2026-05-17 15:25:13 +0800

    feat: project setup with dependencies and config defaults

---
+++ config.py
@@ -0,0 +1,18 @@
+"""Default configuration constants."""
+
+DEFAULTS = {
+    "min_reward_usd": 100.0,
+    "max_spread_cents": 3.0,
+    "min_price_cents": 10.0,
+    "max_price_cents": 50.0,
+    "min_settlement_days": 4,
+    "stop_loss_pct": 15.0,
+    "scan_interval_sec": 30,
+    "fill_check_interval_sec": 5,
+    "cooldown_minutes": 20,
+}
+
+DB_PATH = "market_maker.db"
+HOST = "127.0.0.1"
+PORT = 5000
+SECRET_KEY = None  # Set at runtime from user password
